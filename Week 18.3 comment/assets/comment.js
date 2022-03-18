f55d72a2b2e5787041cf4b81f8ec807ee68647a0
LikaSobolenko <91249749+LikaSobolenko@users.noreply.github.com>
2022-03-18 17:28:57 +0700

    Add files via upload

---
+++ Week 18.3 comment/assets/comment.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    let name = localStorage.getItem('name');
+    if (name != null) {
+        document.getElementById(`name`).value = name;
+    };
+
+    let img = localStorage.getItem('img');
+    if (img != null) {
+        document.getElementById(`img`).src = img;
+    };
+});
+
+function checkSpam() {
+    let comment = document.getElementById(`exampleFormControlTextarea1`).value;
+    let autor = document.getElementById(`name`).value;
+    let comment1 = comment.replace(/viagra|xxx/ig, "***");
+
+    if (localStorage.getItem('name') == null) {
+        localStorage.setItem('name', autor);
+    };
+
+    document.getElementById('result').innerHTML += `<span class = "">${autor}: </span><span>${comment1}</span ><br/>`;
+
+}
+
+function getavatar() {
+    let avatar = document.getElementById('avatar').value;
+    document.getElementById('img').src = avatar;
+
+    if (localStorage.getItem('img') == null) {
+        localStorage.setItem('img', avatar);
+    };
+
+};
